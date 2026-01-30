879e4683d8361113920aa07b9d522b0051f6e529
Maqqr <maqqr@users.noreply.github.com>
2026-01-30 21:33:01 +0200

    Added sprite animation for player

--- Masked Away Light 2/Assets/Game/Scripts/Player/PlayerWalkController.cs
+++ Masked Away Light 2/Assets/Game/Scripts/Player/PlayerWalkController.cs
@@ -1,16 +1,47 @@
 using Masked.GameState;
 using Masked.World;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
 
 namespace Masked.Player
 {
     public class PlayerWalkController : MonoBehaviour
     {
+        public enum WalkDirection
+        {
+            Up = 0,
+            Down = 1,
+            Left = 2,
+            Right = 3,
+        }
+
         public WorldManager WorldManager { get; set; }
+
+        private Animator _animator;
+
+        private static Vector2Int[] _directions = new Vector2Int[]
+        {
+            new Vector2Int(0, 1),   // Up
+            new Vector2Int(0, -1),  // Down
+            new Vector2Int(-1, 0),  // Left
+            new Vector2Int(1, 0),   // Right
+        };
 
         void Start()
         {
+            // Find animator in children
+            _animator = GetComponentInChildren<Animator>();
+            Assert.IsNotNull(_animator, "Animator component not found in children of PlayerWalkController");
+        }
+
+        void Move(WalkDirection direction)
+        {
+            WorldManager.CurrentPosition += _directions[(int)direction];
+            _animator.SetBool("Up", direction == WalkDirection.Up);
+            _animator.SetBool("Down", direction == WalkDirection.Down);
+            _animator.SetBool("Left", direction == WalkDirection.Left);
+            _animator.SetBool("Right", direction == WalkDirection.Right);
         }
 
         void Update()
@@ -21,19 +52,19 @@ namespace Masked.Player
             // Move player with arrow keys
             if (kb.upArrowKey.wasPressedThisFrame)
             {
-                WorldManager.CurrentPosition += new Vector2Int(0, 1);
+                Move(WalkDirection.Up);
             }
-            if (kb.downArrowKey.wasPressedThisFrame)
+            else if (kb.downArrowKey.wasPressedThisFrame)
             {
-                WorldManager.CurrentPosition += new Vector2Int(0, -1);
+                Move(WalkDirection.Down);
             }
-            if (kb.leftArrowKey.wasPressedThisFrame)
+            else if (kb.leftArrowKey.wasPressedThisFrame)
             {
-                WorldManager.CurrentPosition += new Vector2Int(-1, 0);
+                Move(WalkDirection.Left);
             }
-            if (kb.rightArrowKey.wasPressedThisFrame)
+            else if (kb.rightArrowKey.wasPressedThisFrame)
             {
-                WorldManager.CurrentPosition += new Vector2Int(1, 0);
+                Move(WalkDirection.Right);
             }
 
             // Update player transform smoothly to move to currentPosition
